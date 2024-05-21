7088138fdeba0e442cf92851172c711e4a9885bb
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-21 17:26:11 +0200

    fixed remove function and added a new function

--- js/script.js
+++ js/script.js
@@ -2,28 +2,34 @@ const divEl = document.querySelector("div")
 let fiveNumRand = getRandomArray(5 , 1 , 100);
 
 for(let i = 0; i < fiveNumRand.length ; i++){
+    let count;
     let numberEl = document.createElement("p")
     divEl.append(numberEl);
     numberEl.innerHTML = fiveNumRand[i];
+    const removeNumber = setTimeout(removeNumber , 3000 , numberEl);
+    const promt = setTimeout(myPromt , 4000 , fiveNumRand , count)
+
+
 }
 
-const simon = setTimeout(simonSays , 3000 );
-
-function simonSays(){
-    XXXX.innerHTML = "";
-    let count;
-    for(let i = 0; i < 5 ; i++){
+function myPromt(array , countF){
+    for(let i = 0; i < array.length ; i++){
         let insertNumber = Number.parseInt(prompt("Inserisci i numeri che hai visto") , 10);
-        if(XXXX.value === insertNumber){
-            count++;
-            return console.log(insertNumber , count)
-        }
-        else {
-            return console.log("notFound")
-        }
-    }
-
+        if(array === insertNumber){
+            countF++;
+            return console.log(insertNumber , countF)
 }
+        else {
+        return  console.log("notFound")
+    }
+}
+}
+
+
+function removeNumber( firstNumbers){
+    firstNumbers = firstNumbers.remove();
+}
+
 
 
 function getRandomNumber(min , max){
