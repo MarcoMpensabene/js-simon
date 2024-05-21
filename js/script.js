c01d722afe5776cfe953a5ac7c845dc892075a8f
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-21 16:48:31 +0200

    aggiunta funzione che dovrebbe togliere i vecchi numeri e ciclare e controllare i nuovi

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 const divEl = document.querySelector("div")
-
 let fiveNumRand = getRandomArray(5 , 1 , 100);
-console.log(fiveNumRand.length)
+
 for(let i = 0; i < fiveNumRand.length ; i++){
     let numberEl = document.createElement("p")
     divEl.append(numberEl);
     numberEl.innerHTML = fiveNumRand[i];
-
 }
 
+const simon = setTimeout(simonSays , 3000 );
+
+function simonSays(){
+    XXXX.innerHTML = "";
+    let count;
+    for(let i = 0; i < 5 ; i++){
+        let insertNumber = Number.parseInt(prompt("Inserisci i numeri che hai visto") , 10);
+        if(XXXX.value === insertNumber){
+            count++;
+            return console.log(insertNumber , count)
+        }
+        else {
+            return console.log("notFound")
+        }
+    }
+
+}
 
 
 function getRandomNumber(min , max){
